d05bc12416403715b8b53cdee4dda8fe8af35926
Mikhail <kepler186f501@gmail.com>
2023-04-29 18:08:13 +0300

    add check FileNotFound

--- lines-count.py
+++ lines-count.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
 
+import os
 import sys
-
 
 if len(sys.argv) != 2:
     print(f'Usage: lines-counter.py <filename>')
 else:
     filename = sys.argv[1]
-    with open(filename, 'r') as f:
-        data = f.readlines()
-        print(f'{filename}\t{len(data)}')
+    path_to_file = os.path.relpath(filename)
+    if os.path.isfile(path_to_file):
+        with open(path_to_file, 'r') as f:
+            data = f.readlines()
+            print(f'{filename}\t{len(data)}')
+    else:
+        print(f'No such file <{filename}>')
