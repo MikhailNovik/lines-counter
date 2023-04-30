bcb15b1ef5f5ea9d8210af05db77e0dca942e786
Mikhail <kepler186f501@gmail.com>
2023-04-30 15:59:13 +0300

    add handling exceptions

--- lines-count.py
+++ lines-count.py
@@ -5,17 +5,19 @@ import sys
 
 if len(sys.argv) != 2:
     print(f'Usage: lines-counter.py <filename>', file=sys.stderr)
-
 else:
     filename = sys.argv[1]
     path_to_file = os.path.relpath(filename)
     
     try:
-        with open(path_to_file, 'r') as f:
-            data = f.readlines()
-            print(f'{filename}\t{len(data)}')
-    
+        fp = open(path_to_file, 'r')
     except FileNotFoundError:
-        print(f"File is requested but doesn't exist", file=sys.stderr)        
+        print(f"File is requested but doesn't exist", file=sys.stderr)
     except PermissionError:
         print(f'Trying to run an operation without the adequate access rights', file=sys.stderr)
+    
+    else:
+        with fp:
+            data = fp.readlines()
+            print(f'{filename}\t{len(data)}')
+            
